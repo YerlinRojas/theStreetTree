5d6aca226e7deff8bbc040de9510d99ac15a884f
Yerlin ROJAS <yerlinrojas808@gmail.com>
2023-01-18 13:23:13 -0300

    Entrega2 actualizacion js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function saludo(){
+/* function saludo(){
 
     
     alert("Bienvenido")
@@ -9,11 +9,11 @@ saludo();
 
 
 alert("Registrate y Obten beneficios") 
-alert("Te pediremos algunos datos para avanzar")  
+alert("Te pediremos algunos datos para avanzar")   */
 
 /* variable no constant*/
 
-let nombre = prompt ("Ingresar nombre")
+/* let nombre = prompt ("Ingresar nombre")
 let lastname = prompt ("Ingresar apellido")
 let age = parseInt(prompt("Ingresar edad"));
 let telephon = parseInt(prompt("Ingresar telefono"));
@@ -31,19 +31,19 @@ do {
 } while (flag);
 
 
-let datosVar = "Nombre "+ nombre+" Apellido "+ lastname+" Edad "+age+" Telefono "+ telephon   
+let datosVar = "Nombre "+ nombre+" Apellido "+ lastname+" Edad "+age+" Telefono "+ telephon    */
 
 /* variables constant */
-
+/* 
 const dni = parseInt (prompt ("Ingresar DNI"))
 const birthday= parseInt (prompt("Ingrear fecha de nacimiento"))
 const cuil = parseInt (prompt("Ingresar CUIL"))
 const datosNvar=" DNI "+dni+" Cumpleaños "+ birthday+" CUIL "+ cuil
 
-console.log(datosVar+" "+datosNvar)  
+console.log(datosVar+" "+datosNvar)   */
  
 /* Bucle */
-const user= "admin"
+/* const user= "admin"
 const pass= "1234"
 let intentos= 3;
 
@@ -66,18 +66,18 @@ if (u == user && p == pass ){
     }
     
 } 
-
+ */
 /* function arrow  */
 
-const product = prompt("Elija un modelo at,ar, ay")
+/* const product = prompt("Elija un modelo at,ar, ay")
 
 const Precio_Product = {
     at : 1500,
     ar : 3000,
     ay : 2500,
 
-}
-const precio_default = 0
+} */
+/* const precio_default = 0
 
 
 const product_final = Precio_Product[product] || precio_default
@@ -107,5 +107,124 @@ console.log("Descuento  "+prodesc)
 let impoteTotal = restaProd(suma(importeProdu,calcularIva(calcularIva)),desc(desc))
 alert("Importe Total a Pagar " + impoteTotal)
 console.log(impoteTotal)
+ */
+
+/* *********************************ENTREGA 2********************************* */
+
+/* constructor- especificaciones del producto */
+class ropa 
+{ constructor(tipo, marca, material, genero, temporada)
+    {
+    this.tipo = tipo
+    this.marca = marca
+    this.material = material
+    this.genero = genero
+    this.temporada = temporada
+}
+}
+
+/* Se crean los diferentes modelos */
+const modelo01 = new ropa(
+    "campera",
+    "zara",
+    "cuero",
+    "mujer",
+    "invierno",
+)
+
+const modelo02 = new ropa(
+    "campera",
+    "puma",
+    "tela",
+    "hombre",
+    "invierno",
+)
+
+const modelo03 = new ropa(
+    "remera",
+    "nike",
+    "tela",
+    "hombre",
+    "verano",
+)
+
+const modelo04 = new ropa(
+    "remera",
+    "nike",
+    "tela",
+    "hombre",
+    "verano",
+)
+
+
+/* stock en tienda */
+const stock = [
+    {modelo: "modelo01" , precio: 1500},
+    {modelo: "modelo02" , precio: 3000},
+    {modelo: "modelo03" , precio: 2500},
+    {modelo: "modelo04" , precio: 200},
+];
+console.log(stock)
+
+
+/* carrito de compras vacio*/
+ const carrito= []
+
+
+/* filter - busca en stock*/
+const search = stock.filter((prod)=> prod.modelo.includes("modelo01"))
+console.log(search) 
+
+
+/* metodo push- agrega al array - carrito */
+carrito.push(
+    {modelo: "modelo02" , precio: 3000},
+    {modelo: "modelo03" , precio: 2500},
+    {modelo: "modelo04" , precio: 200},
+
+)
+
+
+/* forEach- elementos dentro del carrito*/
+ carrito.forEach((productos) => {
+    console.log(productos);
+}
+)
+
+/* funcion para iva */
+function precioIva (precio) {
+    let result = precio * 1.21
+    return result
+    }
+
+
+/* forEach - precio mas iva */
+carrito.forEach(prod => prod.precio = precioIva(prod.precio))
+console.log(carrito)
+
+
+
+/* map- hacer un descuento y mostrar un nuevo array*/
+const precioDesc = carrito.map((prod1) => {
+    
+    return{
+        nombre: prod1.modelo,
+        precio:prod1.precio-(prod1.precio * 0.10)
+
+    }
+
+}
+)
+
+console.log(precioDesc)
+
+/* reduce- hacer una sumatoria total de los productos */
+const importeTotal = precioDesc.reduce ((acumulador, precioDesc) => acumulador+ precioDesc.precio, 0);
+console.log(importeTotal)
+
+
+/* splice - para eliminar elementos dentro del array- uno o varios-  */
+
+
 
 
