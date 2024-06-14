cfc22222cb76bd7d536da646da1618cce6e35480
Yerlin Rojas <yerlinrojas808@gmail.com>
2024-06-14 17:05:50 -0300

    Update filtro.js

--- filtro.js
+++ filtro.js
@@ -1,124 +1,88 @@
-const productFiltrado = JSON.parse(localStorage.getItem('productoBoton'))
-console.log(productFiltrado)
+const productFiltrado = JSON.parse(localStorage.getItem('productoBoton'));
+    console.log(productFiltrado);
 
-
-const productos = [
-
-    {
+    const productos = [
+      {
         id: "card1",
         titulo: "campera amarilla",
         imagen: "./assets/images/card1_amarillo.png",
-        categoria:{
-            nombre: "kids",
-            id: "kids",
+        categoria: {
+          nombre: "kids",
+          id: "kids",
         },
         precio: 1000,
-
-    },
-
-    {
+      },
+      {
         id: "card2",
         titulo: "botas hombre",
         imagen: "./assets/images/card2_botashombre.png",
-        categoria:{
-            nombre: "hombre",
-            id: "hombre",
+        categoria: {
+          nombre: "hombre",
+          id: "hombre",
         },
         precio: 1500,
-
-    },
-
-    {
+      },
+      {
         id: "card3",
         titulo: "conjunto deportivo",
         imagen: "./assets/images/card3_Cdeporhombre.png",
-        categoria:{
-            nombre: "hombre",
-            id: "hombre",
+        categoria: {
+          nombre: "hombre",
+          id: "hombre",
         },
         precio: 2000,
-
-    },
-
-    {
+      },
+      {
         id: "card4",
         titulo: "conjuto deportivo",
         imagen: "./../assets/images/card4_mujerdeportivoconunto.png",
-        categoria:{
-            nombre: "mujer",
-            id: "mujer",
+        categoria: {
+          nombre: "mujer",
+          id: "mujer",
         },
         precio: 2000,
+      },
+    ];
 
-    },
+    const contenedorProductos = document.querySelector("#contenedorProductos");
 
-
-] 
-
-const contenedorProductos = document.querySelector("#contenedorProductos")
-const botonesCategorias = document.querySelectorAll(".boton-categoria"); 
-
-function cargarProductos (productosElegidos) {
-
-    contenedorProductos.innerHTML="";
-productosElegidos.forEach(producto => {
+    function cargarProductos(productosElegidos) {
+      contenedorProductos.innerHTML = "";
+      productosElegidos.forEach((producto) => {
         const div = document.createElement("div");
-         div.classList.add("producto");
-         div.innerHTML = 
-         `
-         <div class="card-bodyy">
-         <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
-         <div class="producto-detalle">
-           <h5 class="producto-titulo">${producto.titulo}</h5>
-           <p class="producto-precio">${producto.precio}</p>
-           <button class="producto-agregar" id="${producto.id}">Agregar</button>
-         </div>
-         </div>
-         `
-         contenedorProductos.append(div);
+        div.classList.add("producto");
+        div.innerHTML = `
+          <div class="card-bodyy">
+            <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+            <div class="producto-detalle">
+              <h5 class="producto-titulo">${producto.titulo}</h5>
+              <p class="producto-precio">$${producto.precio}</p>
+              <button class="producto-agregar" id="${producto.id}">Agregar</button>
+            </div>
+          </div>
+        `;
+        contenedorProductos.append(div);
+      });
+    }
 
-           });
-        }
+    cargarProductos(productFiltrado || productos);
 
-
-cargarProductos(productFiltrado) 
-
-
-
-/* let lista = document.getElementById("lista") */
-/* contenedorProductos.innerHTML=""; */
-
-
-
-/* innerHtml segun api */
-
- /*        <div class="card-bodyy">
-         <img class="producto-imagen" src="${info.image}" >
-         <div class="producto-detalle">
-           <h5 class="producto-titulo">${info.description}</h5>
-           <p class="producto-precio">${info.price}</p>
-         </div>
-         </div> */
-
-
-/* intento con fetch mas innerHtml segun mi cards */
- fetch('https://fakestoreapi.com/products?limit=2')
-            .then(res=>res.json())
-            
-           .then(productosElegidos =>{
-        productosElegidos.forEach(producto=>{
-        const div = document.createElement(`div`)
-        div.innerHTML=`
-        <div class="card-bodyy">
-        <img class="producto-imagen" src="${producto.image}" alt="${producto.description}">
-        <div class="producto-detalle">
-          <h5 class="producto-titulo">${producto.description}</h5>
-          <p class="producto-precio">${producto.price}</p>
-          <button class="producto-agregar" id="${producto.id}">Agregar</button>
-        </div>
-        </div>
-
-        `
-        contenedorProductos.append(div);  
-    });
-})
+    fetch('https://fakestoreapi.com/products?limit=2')
+      .then(res => res.json())
+      .then(productosElegidos => {
+        productosElegidos.forEach(producto => {
+          const div = document.createElement('div');
+          div.classList.add('producto');
+          div.innerHTML = `
+            <div class="card-bodyy">
+              <img class="producto-imagen" src="${producto.image}" alt="${producto.title}">
+              <div class="producto-detalle">
+                <h5 class="producto-titulo">${producto.title}</h5>
+                <p class="producto-precio">$${producto.price}</p>
+                <button class="producto-agregar" id="${producto.id}">Agregar</button>
+              </div>
+            </div>
+          `;
+          contenedorProductos.append(div);
+        });
+      });
